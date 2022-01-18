c19a07b3c08b12524b39f99c1f6ca8933d2a2f45
akaki <akakibenidze17@gmail.com>
2022-01-18 21:06:46 +0400

    first

---
+++ app/src/main/java/com/example/countryquizapp/RecoverActivity.kt
@@ -0,0 +1,11 @@
+package com.example.countryquizapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RecoverActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_recover)
+    }
+}
